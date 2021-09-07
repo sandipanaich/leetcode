615cb2483c15237c87153b574d5d57b770ad7bfc
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2021-09-07 20:29:55 +0530

    Richest Customer Wealth. Close #9.

--- src/com/sandipan/aich/easy/RichestCustomerWealth.java
+++ src/com/sandipan/aich/easy/RichestCustomerWealth.java
@@ -2,7 +2,7 @@ package com.sandipan.aich.easy;
 
 public class RichestCustomerWealth {
 	public int maximumWealth(int[][] accounts) {
-
+		
 		int max = 0;
 		for (int[] array : accounts) {
 			int sum = 0;
