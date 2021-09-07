e296bb7b74068907bb4b73244e054888b66b207a
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2021-09-07 20:19:41 +0530

    Build Array From Permutation. Close #8.

---
+++ src/com/sandipan/aich/easy/BuildArrayFromPermutation.java
@@ -0,0 +1,15 @@
+package com.sandipan.aich.easy;
+
+public class BuildArrayFromPermutation {
+
+	public int[] buildArray(int[] nums) {
+
+		int[] ans = new int[nums.length];
+
+		for (int i = 0; i < nums.length; i++) {
+			ans[i] = nums[nums[i]];
+		}
+
+		return ans;
+	}
+}
