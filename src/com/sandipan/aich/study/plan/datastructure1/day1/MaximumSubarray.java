35fe05ec7d642de3453389e5f259e56bc41372f5
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2021-09-07 21:53:52 +0530

    Maximum Subarray. Close #14.

---
+++ src/com/sandipan/aich/study/plan/datastructure1/day1/MaximumSubarray.java
@@ -0,0 +1,17 @@
+package com.sandipan.aich.study.plan.datastructure1.day1;
+
+public class MaximumSubarray {
+	
+	public int maxSubArray(int[] array) {
+		int size = array.length;
+		int max = array[0];
+		int max_current = array[0];
+
+		for (int i = 1; i < size; i++) {
+			max_current = Math.max(array[i], max_current + array[i]);
+			if (max < max_current)
+				max = max_current;
+		}
+		return max;
+	}
+}
