2a4cb9a79e199f32b2636769fde91ebe6b254bce
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2021-09-09 21:09:33 +0530

    Intersection of Two Arrays II. Close #21

---
+++ src/com/sandipan/aich/study/plan/datastructure1/day1/IntersectionOfTwoArraysII.java
@@ -0,0 +1,31 @@
+package com.sandipan.aich.study.plan.datastructure1.day1;
+
+public class IntersectionOfTwoArraysII {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i : nums1)
+        {
+            if(!map.containsKey(i))
+                map.put(i, 1);
+            else
+                map.put(i, map.get(i) + 1);
+        }
+        
+        for(int i : nums2)
+        {
+            if(map.containsKey(i))
+            {
+                list.add(i);
+                if(map.get(i) == 1)
+                    map.remove(i);
+                else
+                    map.put(i, map.get(i) -1);
+            }
+        }
+        int[] array = list.stream().mapToInt(i->i).toArray();
+        return array;
+    }
+}
+}
